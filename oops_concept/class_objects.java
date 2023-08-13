f7a312166887c6db39db1e46bb6b8c9b768e65c8
Shashank Bhadouriya <shshankmhow@gmail.com>
2023-08-13 20:08:56 +0530

    class and objects example

--- oops_concept/class_objects.java
+++ oops_concept/class_objects.java
@@ -1,23 +1,23 @@
-class hi{
-  void he(){
-    System.out.println("hi he");
-  }
+// this is class a blueprint
 
-public class class_object_static{
-  public void check(int n){
-    if(n>0){
-      System.out.println("hru hgit ");
-    }
-    else{
-      System.out.println("no");
-    }
-  }
+class Pen{
+  String color;
+  String type; // gel ball etc
 
-  public void main(String[] args){
-    //class_object_static a = new class_object_static();
-    check(7);
-    hi.class_object_static b = new hi.class_object_static();
-
+  public void write(){
+    System.out.println(color+" "+type+"pen is writing");
   }
 }
+
+public class class_objects{
+  public static void main(String[] args) {
+    Pen pen1 = new Pen();
+    pen1.color = "green";
+    pen1.type = "fountain";
+
+    // now to call method of write
+
+    pen1.write();
+
+  }
 }
