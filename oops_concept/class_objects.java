7f79c2e1219d421209e6beaa69c2cf1504e11d6d
Shashank Bhadouriya <shshankmhow@gmail.com>
2023-08-13 19:47:14 +0530

    test form commit

---
+++ oops_concept/class_objects.java
@@ -0,0 +1,23 @@
+class hi{
+  void he(){
+    System.out.println("hi he");
+  }
+
+public class class_object_static{
+  public void check(int n){
+    if(n>0){
+      System.out.println("hello");
+    }
+    else{
+      System.out.println("no");
+    }
+  }
+
+  public void main(String[] args){
+    //class_object_static a = new class_object_static();
+    check(7);
+    hi.class_object_static b = new hi.class_object_static();
+    
+  }
+}
+}
