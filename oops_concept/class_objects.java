b935d7c74a68b93925c64453f0f7eb61e85de1b5
Shashank Bhadouriya <shshankmhow@gmail.com>
2023-08-20 12:08:50 +0530

    user defined content class objects

--- oops_concept/class_objects.java
+++ oops_concept/class_objects.java
@@ -1,19 +1,27 @@
 // this is class a blueprint
 
+import java.util.Scanner;
+
 class Pen{
   String color;
   String type; // gel ball etc
-
+  String content;
   public void write(){
-    System.out.println(color+" "+type+"pen is writing");
+    System.out.println(color+" "+type+"pen is writing"+ " " + content);
   }
 }
 
 public class class_objects{
+
+  static Scanner scn = new Scanner(System.in);
   public static void main(String[] args) {
+
     Pen pen1 = new Pen();
     pen1.color = "green";
     pen1.type = "fountain";
+    System.out.println("enter content: ");
+
+    pen1.content = scn.nextLine();
 
     // now to call method of write
 
---
+++ oops_concept/class_objects_user_defined.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+class Pen {
+    String color;
+    String type;
+    String content;
+    public void write(){
+      System.out.println(color + " "+ type+" "+content);
+    }
+  }
+
+public class class_objects_user_defined {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    Pen userPen = new Pen();
+
+    System.out.println("enter color: ");
+    userPen.color = scn.nextLine();
+
+    System.out.println("enter type: ");
+    userPen.type = scn.nextLine();
+
+    System.out.println("enter content: ");
+    userPen.content = scn.nextLine();
+
+    userPen.write();
+
+
+  }
+}
